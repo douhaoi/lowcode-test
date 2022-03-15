97e6b795b480f7611590f4f2934b777da61ff71f
douhaoi <douhaoi@126.com>
2022-03-15 23:56:22 +0800

    feat: init project

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="font-sans p-2">Home</div>
+  <div>{{ val }}</div>
+  <button @click="handleAdd()">增加</button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "WidgetLayoutHome",
+};
+</script>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const val = ref(2022);
+
+function handleAdd() {
+  val.value++;
+}
+</script>
