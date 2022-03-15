ff7973663f173af25d50061932c6d708f154b911
douhaoi <douhaoi@126.com>
2022-03-16 00:56:56 +0800

    refactor: change style import

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,3 @@
+import 'element-plus/dist/index.css'
+import "./index.scss";
+import 'virtual:windi.css'
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import ElementPlus from 'element-plus'
+import ElementPlus from "element-plus";
 
-import 'element-plus/dist/index.css'
-import "@/assets/styles/index.scss";
-import 'virtual:windi.css'
+import "@/assets/styles";
 
 const app = createApp(App);
 app.use(router).use(ElementPlus);
